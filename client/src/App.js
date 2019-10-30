02789b5f805f3e8ebf4022a16f9dae0040934747
Robert Stewart <rstewart414@gmail.com>
2019-10-30 13:33:43 -0400

    mvp and stretch done

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,58 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
 
 import SavedList from './Movies/SavedList';
+import MovieList from './Movies/MovieList';
+import Movie from './Movies/Movie';
 
-const App = () => {
-  const [savedList, setSavedList] = useState( [] );
+export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+        savedList: []
+    };
+}
 
-  const addToSavedList = movie => {
-    setSavedList( [...savedList, movie] );
-  };
+addToSavedList = movie => {
+    const savedList = this.state.savedList
+        // const savedListIds = savedList.map(i => i.id);
 
-  return (
-    <div>
-      <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
-    </div>
-  );
+    savedList.map(i => i.id).includes(movie.id) ? alert(`${movie.title} is already in list`) : savedList.push(movie);
+    this.setState({ savedList });
 };
+removeFromSavedList = movie => {
+  let savedList = this.state.savedList.filter(m => m !== movie);
 
-export default App;
+  this.setState({ savedList });
+
+  alert(`${movie.title} was removed from the saved list`);
+}
+
+clearList = () => {
+  this.setState({
+      savedList: []
+  })
+}
+
+render() {
+  return ( <
+      div >
+      <
+      SavedList list = { this.state.savedList }
+      remove = { this.removeFromSavedList }
+      clear = { this.clearList }
+      /> <
+      Route exact path = '/'
+      component = { MovieList }
+      />
+
+      <
+      Route path = '/movies/:id'
+      render = {
+          props => < Movie {...props }
+          save = { this.addToSavedList }
+          />} / >
+          </div>
+      );
+  }
+} 
--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,13 +1,37 @@
-import React from 'react';
+import React, { Component, Fragment } from 'react';
+import { Link } from 'react-router-dom';
 
-const SavedList = props => (
-  <div className="saved-list">
-    <h3>Saved Movies:</h3>
-    {props.list.map(movie => (
-      <span className="saved-movie">{movie.title}</span>
-    ))}
-    <div className="home-button">Home</div>
-  </div>
-);
+export default class SavedList extends Component {
+  constructor(props) {
+    super(props);
+  }
 
-export default SavedList;
+  render() {
+    return (
+      <div>
+      <div className="saved-list">
+        <h3>Saved Movies:</h3>
+        {this.props.list.map((movie, i) => (
+          <Fragment key={i}>
+            <Link to={`/movies/${movie.id}`} key={i}>
+              <span className="saved-movie">{movie.title}</span>
+            </Link>
+            <div onClick={() => this.props.remove(movie)}>
+            x
+            </div>
+          </Fragment>
+        ))}
+
+
+        <div className="home-button">
+          <button onClick={this.props.clear}>Clear List</button>
+          <br/>
+          <Link to='/'>
+            Home
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
+}
